1c125004a3775a3cf624a7ba56cace73888002cc
Ripley White <pearlcodes@outlook.com>
2023-05-30 02:40:03 +0100

    I FUCKED THE ENTIRE API LMFAO

--- crates/quark/src/impl/mongo/mod.rs
+++ crates/quark/src/impl/mongo/mod.rs
@@ -59,7 +59,7 @@ impl Deref for MongoDb {
 
 impl MongoDb {
     pub fn db(&self) -> mongodb::Database {
-        self.database("frenzy")
+        self.database("revolt")
     }
 
     pub fn col<T>(&self, collection: &str) -> mongodb::Collection<T> {
